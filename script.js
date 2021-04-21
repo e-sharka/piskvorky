ba3d9a96628539e2c72182d1bc634f181c16a5af
Šárka Princová Maroušková <sarka.marouskova@gmail.com>
2021-04-21 14:24:54 +0200

    Hra: Finální verze úkolu 4/5

--- script.js
+++ script.js
@@ -15,17 +15,3 @@ document.querySelectorAll('.hraci-pole__btn').forEach((btn) => {
     }
   });
 });
-
-/*const displayCircle = (event) => {
-  event.target.classList.add('board__field--circle');
-};
-for (let i = 0; i < buttons.length; i += 1) {
-  buttons[i].addEventListener('click', displayCircle);
-}
-
-const displayCross = (event) => {
-  event.target.classList.add('board__field--cross');
-};
-for (let i = 0; i < buttons.length; i += 1) {
-  buttons[i].addEventListener('click', displayCross);
-} */
