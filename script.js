355260e3b18e954072e768611a9dbf468d25a483
Šárka Princová Maroušková <sarka.marouskova@gmail.com>
2021-04-27 12:33:32 +0200

    Hra: Upravené střídání ikonek u textu HRAJE

--- script.js
+++ script.js
@@ -6,11 +6,11 @@ document.querySelectorAll('.hraci-pole__btn').forEach((btn) => {
   btn.addEventListener('click', () => {
     if (kdoJeNaTahu === 'circle') {
       btn.classList.add('board__field--circle');
-      document.querySelector('.ikona-hrac').src = 'obrazky/circle.svg';
+      document.querySelector('.ikona-hrac').src = 'obrazky/cross.svg';
       kdoJeNaTahu = 'cross';
     } else {
       btn.classList.add('board__field--cross');
-      document.querySelector('.ikona-hrac').src = 'obrazky/cross.svg';
+      document.querySelector('.ikona-hrac').src = 'obrazky/circle.svg';
       kdoJeNaTahu = 'circle';
     }
   });
